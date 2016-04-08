978664330faa9e5aa622b01b527f174d34d44854
Angel Suaste <anglglipe@gmail.com>
2016-04-08 09:19:34 -0500

    Buton siguiente creado y encontrado

--- app/src/main/java/com/tecproject/mdproject/Teoria_texto.java
+++ app/src/main/java/com/tecproject/mdproject/Teoria_texto.java
@@ -2,13 +2,14 @@ package com.tecproject.mdproject;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.speech.tts.TextToSpeech;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Teoria_texto extends AppCompatActivity {
     TextView tv;
+    Button next;
     String texto = "";
     SQLiteDatabase db= null;
     Cursor cursor = null;
@@ -20,6 +21,7 @@ public class Teoria_texto extends AppCompatActivity {
         setContentView(R.layout.activity_teoria_texto);
 
         tv = (TextView)findViewById(R.id.textView);
+        next = (Button)findViewById(R.id.btnNext);
 
         db = this.openOrCreateDatabase("BaseDatos.sqlite", MODE_PRIVATE, null);
 
