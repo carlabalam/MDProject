521d7785e6e269afddfd91d81cb928286dfb0596
Angel Suaste <anglglipe@gmail.com>
2016-04-08 09:21:43 -0500

    main activity modificado para que este limpio y sin codigo basura

--- app/src/main/java/com/tecproject/mdproject/MainActivity.java
+++ app/src/main/java/com/tecproject/mdproject/MainActivity.java
@@ -1,14 +1,10 @@
 package com.tecproject.mdproject;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,57 +12,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    Button b1, b2, b3, b4, banterior, bsiguiente;
-
-    TextView tv;
-    String texto = "";
-    SQLiteDatabase db= null;
-    Cursor cursor = null;
-    int contador =0;
+    Button b1, b2, b3, b4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        tv = (TextView)findViewById(R.id.textView);
         b1 =(Button)findViewById(R.id.bttema1);
-        banterior =(Button)findViewById(R.id.bAnterior);
-        bsiguiente=(Button)findViewById(R.id.bSiguiente);
-
-        db = this.openOrCreateDatabase("BaseDatos.sqlite", MODE_PRIVATE, null);
-        ejecutaSQL();
-        muestraTabla();
-        db.close();
-        tv.append(texto);
-
-
-
-        /*BaseDeDatos myDbHelper = new BaseDeDatos(this);
-
-        try {
-
-            myDbHelper.createDataBase();
-
-        } catch (IOException ioe) {
-
-            throw new Error("Unable to create database");
-
-        }
-
-        try {
-
-            myDbHelper.openDataBase();
-
-        }catch(SQLException sqle){
-
-            throw sqle;
-
-        }*/
 
 
         //b1=(Button) findViewById(R.id.bttema1);
@@ -85,30 +40,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
-
-        banterior.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-
-
-            }
-        });
-
-        bsiguiente.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                contador++;
-
-
-            }
-
-
-
-
-
-        });
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -124,32 +55,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
 
     }
-
-
-
-
-
-    private void ejecutaSQL() {
-        cursor = db.rawQuery("select * from BancoTextos", null);
-    }
-
-    private void muestraTabla() {
-
-
-        //int numeroDeFilas = cursor.getCount();
-
-        cursor.moveToFirst();
-        //for (int i = 1; i <= numeroDeFilas; i++){
-            int id = cursor.getInt(3);
-            String titulo = cursor.getString(3);
-            texto = texto + "\n " + id + ". " + titulo;
-            cursor.moveToNext();
-        //}
-    }
-
-
-
-
 
     @Override
     public void onBackPressed() {
