a74b3cf7abf57417d5ab858c1e626f9abacd0d46
Angel Suaste <anglglipe@gmail.com>
2016-05-29 04:42:27 -0500

    Listo despliegue de imagenes desde la bd para el tema 1

--- app/src/main/java/com/tecproject/mdproject/Teoria_texto.java
+++ app/src/main/java/com/tecproject/mdproject/Teoria_texto.java
@@ -11,7 +11,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.io.ByteArrayInputStream;
 
 public class Teoria_texto extends AppCompatActivity {
     int id_secuencia = 0;
@@ -21,13 +22,13 @@ public class Teoria_texto extends AppCompatActivity {
     private Button next, atras, finalizar;
     private String texto = "";
     SQLiteDatabase db = null;
-    Cursor cursor = null;
-    Cursor cursor2 = null;
-    Cursor cursor3 = null;
-    int numRows;
-    public byte [] imagenData;
-    public int longArray;
-    public int total=0;
+    private Cursor cursor = null;
+    private Cursor cursor2 = null;
+    private Cursor cursor3 = null;
+    private int numRows;
+    private byte[] imagenData;
+    private int campoIdPicture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,9 +56,10 @@ public class Teoria_texto extends AppCompatActivity {
         ejecutaSQL();
         muestraTabla();
         tv.setText(texto);
-        /*if(muestraTabla()){
-            imagen.setImageBitmap(MapearImagen());
-        }*/
+        if (campoIdPicture != 0){
+            imagen.setImageBitmap(consultarImagen());
+            campoIdPicture = 0;
+        }
         atras.setVisibility(View.GONE);
 
 
@@ -68,7 +70,6 @@ public class Teoria_texto extends AppCompatActivity {
 
                 id_secuencia++;
 
-
                 if (id_secuencia > 0){
                     atras.setVisibility(View.VISIBLE);
                     atras.setEnabled(true);
@@ -78,10 +79,10 @@ public class Teoria_texto extends AppCompatActivity {
                 ejecutaSQL();
                 muestraTabla();
                 tv.setText(texto);
-                if(muestraTabla()){
-                    imagen.setImageBitmap(MapearImagen());
+                if (campoIdPicture != 0){
+                    imagen.setImageBitmap(consultarImagen());
+                    campoIdPicture = 0;
                 }
-
                 if(id_secuencia == numRows-1){
                     next.setVisibility(View.INVISIBLE);
                     finalizar.setVisibility(View.VISIBLE);
@@ -97,21 +98,22 @@ public class Teoria_texto extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                    id_secuencia--;
+                id_secuencia--;
                 next.setVisibility(View.VISIBLE);
                 finalizar.setVisibility(View.GONE);
 
-                    if(id_secuencia==0){
-                        atras.setVisibility(View.GONE);
-                    }
+                if(id_secuencia==0){
+                    atras.setVisibility(View.GONE);
+                }
 
-                    texto = "";
-                    ejecutaSQL();
-                    muestraTabla();
-                    tv.setText(texto);
-                    if(muestraTabla()){
-                        imagen.setImageBitmap(MapearImagen());
-                    }
+                texto = "";
+                ejecutaSQL();
+                muestraTabla();
+                tv.setText(texto);
+                if (campoIdPicture != 0){
+                    imagen.setImageBitmap(consultarImagen());
+                    campoIdPicture = 0;
+                }
             }
         });
 
@@ -129,51 +131,47 @@ public class Teoria_texto extends AppCompatActivity {
     }
 
     private void ejecutaSQL() {
-        cursor = db.rawQuery("SELECT BancoTextos.Secuencia,BancoTextos.Textos,SubTemas.NomSubtema,BancoImagenes.Imagen,BancoImagenes._id FROM BancoTextos LEFT JOIN SubTemas ON BancoTextos.SubTemas_id = SubTemas._id LEFT JOIN BancoImagenes ON BancoTextos.BancoImagenes_id = BancoImagenes._id WHERE BancoTextos.SubTemas_id == " + id_subtema + " and BancoTextos.Secuencia == "+ id_secuencia, null);
+        cursor = db.rawQuery("SELECT * FROM BancoTextos WHERE SubTemas_id == " + id_subtema + " AND Secuencia == " + id_secuencia, null);
         /*cursor = db.rawQuery("SELECT * FROM BancoTextos WHERE SubTemas_id == " + id_subtema + "  AND Secuencia ==" + id_secuencia, null);*/
-
     }
-    //1 INT
-    //2 STRING
-    //3 STRING
-    //4 ARRAY BYTE
-    //5 ID
 
-    private boolean muestraTabla() {
-
-        boolean bandera = false;
+    private void muestraTabla() {
         cursor.moveToFirst();
-        String texto = cursor.getString(1);
-        this.texto = texto + "\n " + texto;
+        String teoria = cursor.getString(3);
+        texto = texto + "\n " + teoria;
 
-
-        if (cursor.getBlob(3) != null){
-            bandera = true;
-            byte[] imagenData = cursor.getBlob(3);
-            int largoArray = imagenData.length;
-            Toast.makeText(this, "Largo array" + largoArray, Toast.LENGTH_SHORT).show();
-
+        if (cursor.getInt(1) != 0){
+            campoIdPicture = cursor.getInt(1);
         }
-        Toast.makeText(this, "No HAY IMAGEN", Toast.LENGTH_SHORT).show();
         cursor.moveToNext();
-
-        return bandera;
     }
 
-
-
-    /*public static Bitmap getImage(byte[] imagen) {
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(imagen);
+    private Bitmap consultarImagen() {
+        //Toast.makeText(Teoria_texto.this, "entrando en consultar imagene y el valor de campoIdPic es :" + campoIdPicture, Toast.LENGTH_SHORT).show();
+        cursor3 = db.rawQuery("SELECT * FROM BancoImagenes WHERE _id == " + campoIdPicture, null);
+        cursor3.moveToFirst();
+        imagenData = cursor3.getBlob(2);
+        int largo = imagenData.length;
+        //Toast.makeText(Teoria_texto.this, "largo es de " + largo, Toast.LENGTH_SHORT).show();
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(imagenData);
         Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
         return bitmap;
-    }*/
-    public Bitmap MapearImagen(){
+    }
+
+
+    public static Bitmap getImage(byte[] imagen) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(imagen);
+        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+        return bitmap;
+    }
+
+   /* public Bitmap MapearImagen(){
 
         Bitmap bitmap = BitmapFactory.decodeByteArray(this.imagenData , 0, this.imagenData .length);
 
         return bitmap;
-    }
-
+    }*/
 
     private void set_id_subtema(int id_subtema) {
         this.id_subtema = id_subtema;
