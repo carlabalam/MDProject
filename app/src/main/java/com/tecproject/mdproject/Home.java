6ccb66098b4d547798fdee5f456ed6a27283953a
Angel Suaste <anglglipe@gmail.com>
2016-06-07 01:16:37 -0500

    bd actualizado

--- app/src/main/java/com/tecproject/mdproject/Home.java
+++ app/src/main/java/com/tecproject/mdproject/Home.java
@@ -2,9 +2,7 @@ package com.tecproject.mdproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -45,7 +43,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
             }
         });
 
-        b2.setOnClickListener(new View.OnClickListener(){
+ /*       b2.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
@@ -65,7 +63,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
                 startActivity(intent);
 
             }
-        });
+        });*/
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
